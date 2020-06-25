f9e78b15d4df39d23f6b72ebfd4e0c010f5595d1
feihoo87 <xfh1987@gmail.com>
2020-06-25 12:19:18 +0800

    add test

--- tests/test_waveform.py
+++ tests/test_waveform.py
@@ -1,3 +1,10 @@
+import numpy as np
+
+from waveforms import *
+
+
 def test_waveform():
-    assert True
-    
+    wav = cos(1)
+    t = np.linspace(-10,10,1001)
+
+    assert np.allclose(wav(t), np.cos(t))
