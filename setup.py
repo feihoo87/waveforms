27fd02465ed6c849285fab696cde8b04a519d6d6
feihoo87 <xfh1987@gmail.com>
2021-04-11 17:50:48 +0800

    add 3.9 in classifiers

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     project_urls={  # Optional
         'Bug Reports': 'https://github.com/feihoo87/waveforms/issues',
