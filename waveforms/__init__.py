d84aa9902b2a663684da4d824a7625fe5b4c1a69
feihoo87 <xfh1987@gmail.com>
2021-05-07 16:35:45 +0800

    v0.5.0

--- waveforms/__init__.py
+++ waveforms/__init__.py
@@ -1,2 +1,6 @@
+from .cache import Cache, cache, clear
+from .config import Config
 from .version import __version__
-from .waveform import *
+from .waveform import (D, Waveform, const, cos, cosPulse, exp, gaussian,
+                       mixing, one, poly, registerBaseFunc, registerDerivative,
+                       sign, sin, square, step, zero)
