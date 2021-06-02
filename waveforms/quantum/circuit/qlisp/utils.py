9baab698cdf5620659a5cda449df8c040e1a7b70
feihoo87 <xfh1987@gmail.com>
2021-06-02 18:38:15 +0800

    add DD etc.

---
+++ waveforms/quantum/circuit/qlisp/utils.py
@@ -0,0 +1,50 @@
+from itertools import repeat
+
+import numpy as np
+
+
+def DD(t, gates, pos, Delta=0):
+    seq = ['X/2']
+    i = 0
+    for gate in gates:
+        gap = t * (pos[i] - pos[i - 1]) if i > 0 else t * pos[0]
+        seq.append(('Delay', gap))
+        seq.append(gate)
+        i += 1
+    gap = t * (1 - pos[-1]) if len(pos) > 0 else t
+    seq.append(('Delay', gap))
+    if Delta != 0:
+        seq.append(('P', Delta * t))
+    seq.append('X/2')
+    seq.append('Measure')
+    return seq
+
+
+def UDD(n, t, Delta=0):
+    j = np.arange(n) + 1
+    return DD(t, repeat('Y', times=n),
+              np.sin(np.pi * j / (2 * n + 2))**2, Delta)
+
+
+def CPMG(n, t, Delta=0):
+    j = np.arange(n) + 1
+    return DD(t, repeat('Y', times=n), (j - 0.5) / n, Delta)
+
+
+def CP(n, t, Delta=0):
+    j = np.arange(n) + 1
+    return DD(t, repeat('X', times=n), (j - 0.5) / n, Delta)
+
+
+def Ramsey(t, f=0):
+    return [
+        'X/2', ('Delay', t), ('rfUnitary', np.pi / 2, 2 * np.pi * f * t),
+        'Measure'
+    ]
+
+
+def SpinEcho(t, f=0):
+    return [
+        'X/2', ('Delay', t / 2), ('rfUnitary', np.pi, 2 * np.pi * f * t),
+        ('Delay', t / 2), 'X/2', 'Measure'
+    ]
