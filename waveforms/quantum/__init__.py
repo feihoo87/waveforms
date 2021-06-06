f12293e11482d30c271ff7e52fc54b1e305f5730
feihoo87 <xfh1987@gmail.com>
2021-06-06 16:17:46 +0800

    update namespace

--- waveforms/quantum/__init__.py
+++ waveforms/quantum/__init__.py
@@ -2,3 +2,4 @@ from . import xeb
 from .circuit.qlisp import compile, libraries
 from .circuit.qlisp import std as stdlib
 from .circuit.simulator import applySeq, regesterGateMatrix, seq2mat
+from .tomo import qpt, qptInitList, qst, qst_mle, qstOpList
