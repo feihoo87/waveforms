bfa5eff9464cc9366f2d3c10798e1bbf05536747
feihoo87 <xfh1987@gmail.com>
2021-06-12 23:12:24 +0800

    add test for waveform

--- tests/test_waveform.py
+++ tests/test_waveform.py
@@ -18,6 +18,31 @@ def test_waveform():
     std_sq2 = width / (4 * np.sqrt(np.log(2)))
     assert np.allclose(wav(t), np.exp(-(t / std_sq2)**2), atol=5e-03)
 
+    wav = poly([1, -1 / 2, 1 / 6, -1 / 12])
+    assert np.allclose(wav(t), np.poly1d([-1 / 12, 1 / 6, -1 / 2, 1])(t))
+
+
+def test_op():
+    t = np.linspace(-10, 10, 1001)
+
+    wav = cos(1) + sin(2)
+    assert np.allclose(wav(t), np.cos(t) + np.sin(2 * t))
+    wav = cos(1) - sin(2)
+    assert np.allclose(wav(t), np.cos(t) - np.sin(2 * t))
+    wav = cos(1) * sin(2)
+    assert np.allclose(wav(t), np.cos(t) * np.sin(2 * t))
+    wav = cos(1) / 2
+    assert np.allclose(wav(t), np.cos(t) / 2)
+
+
+def test_simplify():
+    t = np.linspace(-10, 10, 1001)
+    wav = cos(1) * sin(2) * cos(3, 4)
+    wav2 = wav.simplify()
+
+    assert np.allclose(wav(t), np.cos(t) * np.sin(2 * t) * np.cos(3 * t + 4))
+    assert np.allclose(wav2(t), np.cos(t) * np.sin(2 * t) * np.cos(3 * t + 4))
+
 
 def test_shift():
     t = np.linspace(-10, 10, 1001)
