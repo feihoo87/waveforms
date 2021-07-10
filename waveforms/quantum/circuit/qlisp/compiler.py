76e6166565721891f10ef32ec294312fc0ccf38a
feihoo87 <xfh1987@gmail.com>
2021-07-10 19:13:33 +0800

    refactor compiler

--- waveforms/quantum/circuit/qlisp/compiler.py
+++ waveforms/quantum/circuit/qlisp/compiler.py
@@ -1,20 +1,28 @@
-from typing import Optional
+from typing import Optional, Sequence, Union
 
 from .assembler import assembly
 from .config import Config
-from .library import Library
+from .library import Library, libraries
 from .macro import extend_macro, reduceVirtualZ
 from .qasm import qasm_eval
 from .stdlib import std
 
 
-def compile(prog, cfg: Optional[Config] = None, lib: Library = std, **options):
+def compile(prog,
+            cfg: Optional[Config] = None,
+            lib: Union[Library, Sequence[Library]] = std,
+            **options):
     """
     options: 
         qasm_only = True: only compile qasm to qlisp
         no_virtual_z = True: keep P gates as original form.
         no_assembly = True: return simplified qlisp.
     """
+    if isinstance(lib, Library):
+        lib = lib
+    else:
+        lib = libraries(*lib)
+        
     if isinstance(prog, str):
         prog = qasm_eval(prog, lib)
     if 'qasm_only' in options:
