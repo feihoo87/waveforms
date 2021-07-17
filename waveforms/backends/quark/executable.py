8e11b46d31e0d5f93846b8f99ce9e5c8ebc711d5
feihoo87 <xfh1987@gmail.com>
2021-07-17 23:10:41 +0800

    add assymblyData

--- waveforms/backends/quark/executable.py
+++ waveforms/backends/quark/executable.py
@@ -1,5 +1,6 @@
 import numpy as np
 from waveforms.math import getFTMatrix
+from waveforms.math.fit import classifyData, count_to_diag, countState
 
 
 def getCommands(code, signal='state'):
@@ -33,9 +34,9 @@ def getCommands(code, signal='state'):
         ADInfo[ad]['w'].append(w)
         ADInfo[ad]['fList'].append(Delta)
         ADInfo[ad]['tasks'].append(task)
-        dataMap[cbit] = (ad, len(ADInfo[ad]['fList']) - 1, Delta, task)
+        dataMap[cbit] = (ad, len(ADInfo[ad]['fList']) - 1, Delta, task.params)
 
-    for i, (channel, info) in enumerate(ADInfo.items()):
+    for channel, info in ADInfo.items():
         coefficient = np.asarray(info['w'])
         cmds.append((channel + '.coefficient', coefficient))
         cmds.append((channel + '.pointNum', coefficient.shape[-1]))
@@ -47,3 +48,38 @@ def getCommands(code, signal='state'):
             cmds.append((channel + '.IQ', None))
 
     return cmds, dataMap
+
+
+def assymblyData(raw_data, dataMap, signal='state', classify=classifyData):
+    def flattenDictIter(d, prefix=[]):
+        for k in d:
+            if isinstance(d[k], dict):
+                yield from flattenDictIter(d[k], prefix=[*prefix, k])
+            else:
+                yield '.'.join(prefix + [k]), d[k]
+
+    ret = []
+    gate_list = []
+    result = {k: v[0] + 1j * v[1] for k, v in flattenDictIter(raw_data)}
+
+    for cbit in sorted(dataMap):
+        ch, i, Delta, params = dataMap[cbit]
+        gate_list.append({'params': params})
+        try:
+            key = f'{ch}.IQ'
+            ret.append(result[key][:, i])
+        except KeyError:
+            key = f'{ch}.TraceIQ'
+            ret.append(result[key][:, i])
+
+    result = {}
+    result['data'] = np.asarray(ret).T
+
+    if signal in ['state', 'count', 'diag']:
+        result['state'] = classify(result['data'], gate_list, avg=False)
+    if signal in ['count', 'diag']:
+        result['count'] = countState(result['state'])
+    if signal == 'diag':
+        result['diag'] = count_to_diag(result['count'])
+
+    return result
