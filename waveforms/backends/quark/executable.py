de40e6e81b02aa3457ad5004c07f4ee461ff02e4
feihoo87 <xfh1987@gmail.com>
2021-07-19 22:39:05 +0800

    add QuarkExcutor

--- waveforms/backends/quark/executable.py
+++ waveforms/backends/quark/executable.py
@@ -1,6 +1,13 @@
+import pickle
+import threading
+import time
+from typing import NamedTuple
 import numpy as np
 from waveforms.math import getFTMatrix
 from waveforms.math.fit import classifyData, count_to_diag, countState
+from waveforms.sched.scheduler import Executor
+
+from quark import connect
 
 
 def getCommands(code, signal='state'):
@@ -85,3 +92,66 @@ def assymblyData(raw_data, dataMap, signal='state', classify=classifyData):
         result['diag'] = count_to_diag(result['count'])
 
     return result
+
+
+class _connection_pool(NamedTuple):
+    actived: dict
+    disactived: list
+    lock: threading.Lock
+    max_unused: int = 10
+
+
+class QuarkExcutor(Executor):
+    def __init__(self, host):
+        from waveforms.backends.quark import set_up_backend
+
+        self.host = host
+        set_up_backend(self.host)
+
+        self._conn_pool = _connection_pool({}, [], threading.Lock())
+        self._gc_thread = threading.Thread(target=self._gc, daemon=True)
+        self._gc_thread.start()
+
+    def _gc(self):
+        """clear unused connections.
+        """
+        while True:
+            time.sleep(60)
+            with self._conn_pool.lock:
+                alived = [t.ident for t in threading.enumerate()]
+                for tid in list(self._conn_pool.actived.keys()):
+                    if tid not in alived:
+                        conn = self._conn_pool.actived.pop(tid)
+                        if len(self._conn_pool.disactived
+                               ) < self._conn_pool.max_unused:
+                            self._conn_pool.disactived.append(conn)
+
+    @property
+    def conn(self):
+        with self._conn_pool.lock:
+            tid = threading.get_ident()
+            if tid not in self._conn_pool:
+                try:
+                    self._conn_pool[tid] = self._conn_pool.disactived.pop()
+                except IndexError:
+                    self._conn_pool[tid] = connect('QuarkServer',
+                                                   host=self.host)
+            return self._conn_pool[tid]
+
+    def feed(self, task_id, task_step, keys, values):
+        self.conn.feed(task_id, task_step, keys, values)
+
+    def free(self, task_id):
+        self.conn.free(task_id)
+
+    def submit(self, task_id, data_template):
+        self.conn.submit(task_id, data_template)
+
+    def result(self, task_id, skip=0):
+        ret = self.conn.result(task_id, skip)
+        if ret is None:
+            return []
+        return ret
+
+    def save(self, path, task_id, data):
+        self.conn.save(path, task_id, pickle.dumps(data))
