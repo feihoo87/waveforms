bb27fe7957ad7aa78d39f61bb268a9d1191a6747
feihoo87 <xfh1987@gmail.com>
2021-08-25 17:24:26 +0800

    add DD etc.

--- waveforms/quantum/__init__.py
+++ waveforms/quantum/__init__.py
@@ -3,5 +3,7 @@ from .circuit.qlisp import compile, libraries
 from .circuit.qlisp import std as stdlib
 from .circuit.qlisp.config import Config, getConfig, setConfig
 from .circuit.qlisp.qlisp import QLispError
+from .circuit.qlisp.utils import (ALLXY, CP, CPMG, DD, UDD, XY4, XY8, XY16,
+                                  Ramsey, SpinEcho)
 from .circuit.simulator import applySeq, regesterGateMatrix, seq2mat
 from .tomo import qpt, qptInitList, qst, qst_mle, qstOpList
