ed02740191d44904fc8bab04f6c9e93e120650c4
feihoo87 <xfh1987@gmail.com>
2022-03-09 01:18:14 +0800

    add fit_decay

--- waveforms/math/fit.py
+++ waveforms/math/fit.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 
 import numpy as np
 from scipy.optimize import curve_fit, minimize
+from scipy.signal import correlate
 from scipy.special import erf
 
 from .geo import EPS, point_in_polygon
@@ -126,6 +127,29 @@ def complex_amp_to_real(s, axis=None):
     k, _ = lin_fit(s.real, s.imag, axis=axis)
     phi = np.arctan(k)
     return np.real(s * np.exp(-1j * phi))
+
+
+def fit_delay(waveform, data, sample_rate, fit=True):
+    t = np.linspace(0, len(data) / sample_rate, len(data), endpoint=False)
+    if isinstance(waveform, np.ndarray):
+        fit = False
+        corr = correlate(waveform, data, mode='same', method='fft')
+    else:
+        corr = correlate(waveform(t), data, mode='same', method='fft')
+    delay = 0.5 * (t[0] + t[-1]) - t[np.argmax(corr)]
+
+    if not fit:
+        return delay
+
+    def fun(delay, t, waveform, sig):
+        if isinstance(delay, (int, float, complex)):
+            ret = -correlate((waveform >> delay)(t), sig, mode='valid')[0]
+            return ret
+        else:
+            return np.array([fun(d, t, waveform, sig) for d in delay])
+
+    ret = minimize(fun, [delay], args=(t, waveform, data))
+    return ret.x[0]
 
 
 def transmon_spectrum(x, EJ, Ec, d, offset, period):
