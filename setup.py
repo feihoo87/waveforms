7b73b1bf05910078a7648b5c5d4b5a409b4fd32c
feihoo87 <xfh1987@gmail.com>
2022-07-01 21:14:21 +0800

    fix building error on windows

--- setup.py
+++ setup.py
@@ -28,12 +28,15 @@ requirements = [
 
 def get_extensions():
     from numpy.distutils.misc_util import get_numpy_include_dirs
+    from pathlib import Path
 
     extensions = [
         Extension(
             'waveforms.math.npufunc',
             ['src/multi_type_logit.c'],
             include_dirs=get_numpy_include_dirs(),
+            library_dirs=[str(Path(p).parent / 'lib') for p in get_numpy_include_dirs()],
+            libraries=["npymath"],
             # extra_compile_args=['-std=c99'],
         ),
         # cythonize("waveforms/math/prime.pyx"),
