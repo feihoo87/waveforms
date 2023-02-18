a31d938e4cb8d418160ea2c7be06056c1243e2dc
feihoo87 <xfh1987@gmail.com>
2023-02-18 22:34:58 +0800

    simplify namespace

--- waveforms/__init__.py
+++ waveforms/__init__.py
@@ -1,9 +1,10 @@
 from numpy import e, pi
 
-from .cache import cache
-from .quantum import (Config, compile, getConfig, libraries, qpt, qptInitList,
-                      qst, qst_mle, qstOpList, setConfig, stdlib)
-from .quantum.transmon import Transmon
+#from .cache import cache
+#from .qlisp import Config, compile, libraries, stdlib
+#from .qlisp.config import getConfig, setConfig
+#from .quantum import qpt, qptInitList, qst, qst_mle, qstOpList
+#from .quantum.transmon import Transmon
 from .version import __version__
 from .waveform import (D, Waveform, chirp, const, cos, cosPulse, cut, exp,
                        function, gaussian, general_cosine, hanning, interp,
