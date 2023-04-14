1e98435d6699723003dcb59ccc2542997d5dd48e
feihoo87 <xfh1987@gmail.com>
2023-04-14 13:05:17 +0800

    improve fit_transmon_spectrum

--- waveforms/math/fit/spectrum.py
+++ waveforms/math/fit/spectrum.py
@@ -1,10 +1,30 @@
 import numpy as np
 from scipy.optimize import curve_fit
 
+from waveforms.quantum.transmon import Transmon
+
+from ._fit import fit
+
+
+def get_2d_spectrum_background(data):
+    """
+    Get background of 2D spectrum.
+
+    Args:
+        data (np.ndarray): 2D spectrum
+
+    Returns:
+        bg (np.ndarray): background
+    """
+    x = np.median(data, axis=0)
+    y = np.median(data, axis=1)
+    x, y = np.meshgrid(x, y)
+    bg = x + y
+    offset = np.median(data - bg)
+    return bg - offset
+
 
 def transmon_spectrum(x, EJ, Ec, d, offset, period):
-    from waveforms import Transmon
-
     x = (x - offset) / period
     q = Transmon(EJ=EJ, Ec=Ec, d=d)
     if isinstance(x, (int, float, complex)):
@@ -18,7 +38,6 @@ def transmon_spectrum(x, EJ, Ec, d, offset, period):
 
 def transmon_spectrum_fast(x, EJ, Ec, d, offset, period):
     from scipy.special import mathieu_a, mathieu_b
-    from waveforms.quantum.transmon import Transmon
 
     x = (x - offset) / period
     q = 0.5 * Transmon._flux_to_EJ(x, EJ, d) / Ec
@@ -38,9 +57,6 @@ def fit_transmon_spectrum(bias,
                           f01_max=None,
                           f01_min=None,
                           alpha=None):
-    from waveforms import Transmon
-
-    x = (bias - offset) / period
 
     f01_max = np.max(f01) if f01_max is None else f01_max
     f01_min = np.min(f01) if f01_min is None else f01_min
@@ -68,3 +84,40 @@ def cavity_spectrum(x, offset, period, fc, f01, g=0.05, d=0.1, Ec=0.24):
     mask = np.abs(a) <= np.abs(b)
     ret[mask] = a[mask]
     return ret + fc
+
+
+def guess_transmon_spectrum(x, y, static_params):
+    x = np.asarray(x)
+    y = np.asarray(y)
+    f01_max = np.max(
+        y) if 'f01_max' not in static_params else static_params.get('f01_max')
+    f01_min = np.min(
+        y) if 'f01_min' not in static_params else static_params.get('f01_min')
+    alpha = -0.24e6 if 'alpha' not in static_params else static_params.get(
+        'alpha')
+
+    offset = np.median(
+        x) if 'offset' not in static_params else static_params.get('offset')
+    period = np.max(x) - np.min(
+        x) if 'period' not in static_params else static_params.get('period')
+
+    q = Transmon(f01_max=f01_max, f01_min=f01_min, alpha=alpha)
+    EJ, Ec, d = q.EJ, q.Ec, q.d
+
+    return {'EJ': EJ, 'Ec': Ec, 'd': d, 'offset': offset, 'period': period}
+
+
+def fit_transmon_spectrum(x,
+                          y,
+                          sigma=None,
+                          static_params=None,
+                          init_params=None):
+    if static_params is None:
+        static_params = {}
+    return fit(transmon_spectrum_fast,
+               x,
+               y,
+               init_params,
+               sigma=sigma,
+               guess=guess_transmon_spectrum,
+               static_params=static_params)
