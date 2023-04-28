895eef7684ec21921b956f86ecd8170e138fe3a2
feihoo87 <xfh1987@gmail.com>
2023-04-28 10:45:47 +0800

    fix namespace

--- waveforms/__init__.py
+++ waveforms/__init__.py
@@ -6,8 +6,9 @@ from numpy import e, pi
 #from .quantum import qpt, qptInitList, qst, qst_mle, qstOpList
 #from .quantum.transmon import Transmon
 from .version import __version__
-from .waveform import (D, Waveform, chirp, const, cos, cosPulse, cut, exp,
-                       function, gaussian, general_cosine, hanning, interp,
-                       mixing, one, poly, registerBaseFunc, registerDerivative,
-                       samplingPoints, sign, sin, sinc, square, step, zero)
+from .waveform import (D, Waveform, chirp, const, cos, cosh, coshPulse,
+                       cosPulse, cut, exp, function, gaussian, general_cosine,
+                       hanning, interp, mixing, one, poly, registerBaseFunc,
+                       registerDerivative, samplingPoints, sign, sin, sinc,
+                       sinh, square, step, zero)
 from .waveform_parser import wave_eval
--- waveforms/version.py
+++ waveforms/version.py
@@ -1,2 +1,2 @@
 """Define version number here and read it from setup.py automatically"""
-__version__ = "1.5.92"
+__version__ = "1.5.93"
