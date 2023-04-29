89285d8d85698cb331ee0ca85ee3c53e6a387b23
feihoo87 <xfh1987@gmail.com>
2023-04-29 10:09:26 +0800

    fix SU(n) generator

--- waveforms/math/group/_SU_n_.py
+++ waveforms/math/group/_SU_n_.py
@@ -2,36 +2,48 @@ import math
 import numpy as np
 
 
-def SUn_generator(n, i):
+def _SUn_generator(n, i):
     assert n > 1 and 0 <= i < 2**n
 
     if i == 0:
-        return np.eye(n, dtype=complex)
-    mat = np.zeros((n, n), dtype=complex)
+        return np.eye(n, dtype=np.int8), 1
+    mat = np.zeros((n, n), dtype=np.int8)
 
     k = math.isqrt(i)
     l = (i - k**2) // 2
-    v = 1j if (i - k**2) % 2 else 1
+    A = 1j if (i - k**2) % 2 else 1
 
     if k != l:
-        mat[k, l] = v
-        mat[l, k] = np.conj(v)
+        mat[k, l] = 1
+        if A == 1:
+            mat[l, k] = 1
+        else:
+            mat[l, k] = -1
     else:
         for j in range(k):
             mat[j, j] = 1
         mat[k, k] = -k
+        A = 1 / np.sqrt(k * (k + 1) / 2)
 
-    return mat
+    return mat, A
 
 
 class _SUGroup():
 
-    def __init__(self, n):
+    def __init__(self, n: int):
+        if n <= 0:
+            raise ValueError("n must be positive")
         self.n = n
 
-    def __getitem__(self, i):
-        return SUn_generator(self.n, i)
+    def __getitem__(self, i: int):
+        if not 0 <= i < 2**self.n:
+            raise IndexError(f"i must be in [0, {2**self.n-1}]")
+        mat, A = _SUn_generator(self.n, i)
+        return A * mat
+
+    def __repr__(self):
+        return f"SU({self.n})"
 
 
-def SU(n):
+def SU(n: int):
     return _SUGroup(n)
