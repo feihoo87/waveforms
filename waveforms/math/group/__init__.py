3ccd72271bc75223ae22a360716747a8ae19e7d2
feihoo87 <xfh1987@gmail.com>
2023-05-12 10:19:12 +0800

    update

--- waveforms/math/group/__init__.py
+++ waveforms/math/group/__init__.py
@@ -1,1 +1,5 @@
 from ._SU_n_ import SU
+from .permutation_group import (AlternatingGroup, Cycles, CyclicGroup,
+                                DihedralGroup, PermutationGroup,
+                                SymmetricGroup, find_permutation, permute,
+                                random_permutation)
