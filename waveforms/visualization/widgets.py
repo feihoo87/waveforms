950c00335e17db1b3e1a85a86097955663fc5da8
feihoo87 <xfh1987@gmail.com>
2023-07-25 00:54:12 +0800

    update DataPicker

--- waveforms/visualization/widgets.py
+++ waveforms/visualization/widgets.py
@@ -5,49 +5,66 @@ import numpy as np
 class DataPicker():
 
     def __init__(self, ax=None):
-        self.points_and_text = []
+        self.points_and_text = {}
+        self.line = None
         if ax is None:
             ax = plt.gca()
         self.ax = ax
         self.ax.figure.canvas.mpl_connect('button_press_event', self.on_click)
+        self.mode = 'pick'
+
+    def on_key_press(self, event):
+        if event.key == 'p':
+            if self.mode != 'pick':
+                self.mode = 'pick'
+            else:
+                self.mode = 'default'
 
     def on_click(self, event):
-        self.ax = plt.gca()
+        if self.mode != 'pick':
+            return
         # 鼠标左键的button值为1
         if event.button == 1 and event.inaxes is not None:
-            point, = self.ax.plot(event.xdata, event.ydata, 'ro')
-            text = self.ax.text(event.xdata,
-                                event.ydata,
-                                f'({event.xdata:.2f}, {event.ydata:.2f})',
+            point = (event.xdata, event.ydata)
+            text = self.ax.text(point[0],
+                                point[1],
+                                f'({point[0]:.2f}, {point[1]:.2f})',
                                 verticalalignment='center')
-            self.points_and_text.append((point, text))
+            self.points_and_text[point] = text
+            x, y = self.get_xy()
+            if self.line is None:
+                self.line, = self.ax.plot(x, y, 'ro')
+            else:
+                self.line.set_data(x, y)
             self.ax.draw()
 
         elif event.button == 3 and event.inaxes is not None:
-            for point, text in self.points_and_text:
-                point_xdata, point_ydata = point.get_data()
-                point_xdata, point_ydata = point_xdata[0], point_ydata[0]
+            for point, text in list(self.points_and_text.items()):
                 point_xdisplay, point_ydisplay = self.ax.transData.transform_point(
-                    [point_xdata, point_ydata])
-                event_xdisplay, event_ydisplay = event.x, event.y
+                    point)
 
-                distance = np.sqrt((point_xdisplay - event_xdisplay)**2 +
-                                   (point_ydisplay - event_ydisplay)**2)
+                distance = np.sqrt((point_xdisplay - event.x)**2 +
+                                   (point_ydisplay - event.y)**2)
                 if distance < 10:
-                    point.remove()
                     text.remove()
-                    self.points_and_text.remove((point, text))
+                    self.points_and_text.pop(point)
+                    if self.points_and_text:
+                        x, y = self.get_xy()
+                        self.line.set_data(x, y)
+                    else:
+                        self.line.remove()
+                        self.line = None
                     self.ax.draw()
                     break
 
     def get_xy(self):
-        x, y = [], []
-        for point, text in self.points_and_text:
-            point_xdata, point_ydata = point.get_data()
-            x.extend(point_xdata)
-            y.extend(point_ydata)
+        if self.points_and_text:
+            data = np.asarray(list(self.points_and_text.keys()))
+            x, y = data[:, 0], data[:, 1]
 
-        index = np.argsort(x)
-        x = np.asarray(x)[index]
-        y = np.asarray(y)[index]
-        return x, y
+            index = np.argsort(x)
+            x = np.asarray(x)[index]
+            y = np.asarray(y)[index]
+            return x, y
+        else:
+            return np.array([]), np.array([])
