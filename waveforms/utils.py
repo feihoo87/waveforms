09e2cedf4ab327fb0ab9c6e934114f1420f8ceaa
feihoo87 <xfh1987@gmail.com>
2023-08-03 22:42:52 +0800

    upgrade freeze

--- waveforms/utils.py
+++ waveforms/utils.py
@@ -7,7 +7,17 @@ import scipy.sparse as sp
 def freeze(x):
     """Freeze a mutable object.
     """
-    if isinstance(x, sp.spmatrix):
+    if isinstance(x, (int, float, complex, str, bytes, type(None))):
+        pass
+    elif isinstance(x, (list, tuple)):
+        return tuple([freeze(y) for y in x])
+    elif isinstance(x, dict):
+        return MappingProxyType({k: freeze(v) for k, v in x.items()})
+    elif isinstance(x, set):
+        return frozenset([freeze(y) for y in x])
+    elif isinstance(x, (np.ndarray, np.matrix)):
+        x.flags.writeable = False
+    elif isinstance(x, sp.spmatrix):
         x.data.flags.writeable = False
         if x.format in {'csr', 'csc', 'bsr'}:
             x.indices.flags.writeable = False
@@ -15,12 +25,6 @@ def freeze(x):
         elif x.format == 'coo':
             x.row.flags.writeable = False
             x.col.flags.writeable = False
-    elif isinstance(x, np.ndarray):
-        x.flags.writeable = False
-    elif isinstance(x, (list, tuple)):
-        return tuple([freeze(y) for y in x])
-    elif isinstance(x, set):
-        return frozenset([freeze(y) for y in x])
-    elif isinstance(x, dict):
-        return MappingProxyType({k: freeze(v) for k, v in x.items()})
+    elif isinstance(x, bytearray):
+        x = bytes(x)
     return x
