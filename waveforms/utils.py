677c2a67df3ec2f3195158469c2bfc2180b6b0fa
feihoo87 <xfh1987@gmail.com>
2023-08-03 22:37:09 +0800

    add freeze

---
+++ waveforms/utils.py
@@ -0,0 +1,26 @@
+from types import MappingProxyType
+
+import numpy as np
+import scipy.sparse as sp
+
+
+def freeze(x):
+    """Freeze a mutable object.
+    """
+    if isinstance(x, sp.spmatrix):
+        x.data.flags.writeable = False
+        if x.format in {'csr', 'csc', 'bsr'}:
+            x.indices.flags.writeable = False
+            x.indptr.flags.writeable = False
+        elif x.format == 'coo':
+            x.row.flags.writeable = False
+            x.col.flags.writeable = False
+    elif isinstance(x, np.ndarray):
+        x.flags.writeable = False
+    elif isinstance(x, (list, tuple)):
+        return tuple([freeze(y) for y in x])
+    elif isinstance(x, set):
+        return frozenset([freeze(y) for y in x])
+    elif isinstance(x, dict):
+        return MappingProxyType({k: freeze(v) for k, v in x.items()})
+    return x
