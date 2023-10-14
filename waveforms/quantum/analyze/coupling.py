ebcf9af51449301a58eaa590a607aab992ab7ef4
feihoo87 <xfh1987@gmail.com>
2023-10-14 22:44:11 +0800

    add effect_coupling_2

--- waveforms/quantum/analyze/coupling.py
+++ waveforms/quantum/analyze/coupling.py
@@ -17,6 +17,42 @@ def effect_coupling(wc, w1, w2, eta, C12, C1, C2):
                     eta + 1) * C12 / np.sqrt(C1 * C2) * np.sqrt(w1 * w2)
 
 
+def effect_coupling_2(wc, w1, w2, wq, wc_off, wc_2, g_2):
+    """
+    Parameters
+    ----------
+    wc : float
+        frequency of coupler
+    w1 : float
+        frequency of qubit 1
+    w2 : float
+        frequency of qubit 2
+    wq : float
+        calibration point, frequency of qubit 1 and 2 when w1 = w2 = wq
+    wc_off : float
+        decoupling frequency of coupler when w1 = w2 = wq
+    wc_2 : float
+        second calibration frequency of coupler when w1 = w2 = wq
+    g_2 : float
+        coupling strength of coupler when w1 = w2 = wq and wc = wc_2
+
+    See also
+    --------
+    https://doi.org/10.1103/PhysRevApplied.10.054062
+    """
+    Delta1 = w1 - wc
+    Delta2 = w2 - wc
+    Sigma1 = w1 + wc
+    Sigma2 = w2 + wc
+    eta = (wc_off / wq)**2 - 1
+
+    a = g_2 / wq / (eta / (1 - (wc_2 / wq)**2) + 1)
+
+    return a * (wc / 4 *
+                (1 / Delta1 + 1 / Delta2 - 1 / Sigma1 - 1 / Sigma2) * eta +
+                eta + 1) * np.sqrt(w1 * w2)
+
+
 def decoupling_frequency_of_coupler(w1, w2, k):
     """
     calculate decoupling frequency of coupler
