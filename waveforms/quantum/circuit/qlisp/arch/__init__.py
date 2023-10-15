c8fc026271f97dd2224ea563b362c0533a2b0cbc
feihoo87 <xfh1987@gmail.com>
2023-10-15 22:54:37 +0800

    deprecate waveforms.quantum.qlisp

--- waveforms/quantum/circuit/qlisp/arch/__init__.py
+++ waveforms/quantum/circuit/qlisp/arch/__init__.py
@@ -3,3 +3,10 @@ from waveforms.qlisp.arch import get_arch, register_arch
 
 from .base import (COMMAND, FREE, PUSH, READ, SYNC, TRIG, WRITE, Architecture,
                    general_architecture)
+import warnings
+
+warnings.warn(
+    'waveforms.quantum.circuit.qlisp is deprecated, please use waveforms.qlisp instead',
+    DeprecationWarning,
+    stacklevel=2,
+)
