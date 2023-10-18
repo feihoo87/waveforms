5efd2dc3d3fbe45f4345bc5ee2f2e0522d18ffa5
feihoo87 <xfh1987@gmail.com>
2023-10-19 00:17:11 +0800

    upgrade imul_paulis

--- waveforms/quantum/clifford/paulis.py
+++ waveforms/quantum/clifford/paulis.py
@@ -27,7 +27,7 @@ def encode_paulis(a: str):
     return (ret << 2) | sign
 
 
-def decode_paulis(a: int):
+def decode_paulis(a: int, N=None):
     code = ['I', 'X', 'Z', 'Y']
     sign = ['  ', ' i', ' -', '-i'][a & 3]
     a >>= 2
@@ -37,10 +37,12 @@ def decode_paulis(a: int):
         a >>= 2
     if not s:
         s.append('I')
+    if N is not None and N > len(s):
+        s.extend(['I'] * (N - len(s)))
     return sign + ''.join(s)
 
 
-def mul_paulis(a, b):
+def mul_paulis(a: int | str, b: int | str):
     if isinstance(a, str):
         a = encode_paulis(a)
     if isinstance(b, str):
@@ -48,14 +50,20 @@ def mul_paulis(a, b):
     return decode_paulis(imul_paulis(a, b))
 
 
-def imul_paulis(a, b):
-    tab = [0, 0, 0, 0, 0, 0, 3, 1, 0, 1, 0, 3, 0, 3, 1, 0]
+def imul_paulis(a: int, b: int):
     sign = (a + b) & 3
-    code = ((a ^ b) | 3) ^ 3
+
     a = a >> 2
     b = b >> 2
-    while a or b:
-        sign += tab[((a & 3) << 2) | (b & 3)]
-        a = a >> 2
-        b = b >> 2
-    return code | sign & 3
+    c = a ^ b
+
+    d = max(a.bit_length(), b.bit_length())
+    d += d % 2
+    m = ((1 << d) - 1) // 3
+
+    l = (a | (a >> 1)) & (b | (b >> 1)) & (c | c >> 1) & m
+    h = (((a >> 1) & b) ^ (c & (c >> 1))) & l
+
+    sign = (h.bit_count() * 2 + l.bit_count() + sign) & 3
+
+    return (c << 2) | sign
