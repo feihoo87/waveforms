8907c34ca11ac8cf744c35a39f7329b92058ccfd
feihoo87 <xfh1987@gmail.com>
2023-11-14 21:30:19 +0800

    update test

--- tests/test_kad.py
+++ tests/test_kad.py
@@ -1,6 +1,7 @@
 import asyncio
 import hashlib
 import random
+import time
 from struct import pack
 
 import pytest
@@ -245,6 +246,7 @@ class TestForgetfulStorage:
     def test_forgetting(self):  # pylint: disable=no-self-use
         storage = ForgetfulStorage(0)
         storage['one'] = 'two'
+        time.sleep(0.1)
         assert storage.get('one') is None
 
     def test_iter(self):  # pylint: disable=no-self-use
@@ -277,20 +279,19 @@ async def test_storing(bootstrap_node):
 
 class TestSwappableProtocol:
 
-    def test_default_protocol(self):  # pylint: disable=no-self-use
+    def test_default_protocol(self, event_loop):  # pylint: disable=no-self-use
         """
         An ordinary Server object will initially not have a protocol, but will
         have a KademliaProtocol object as its protocol after its listen()
         method is called.
         """
-        loop = asyncio.get_event_loop()
         server = Server()
         assert server.protocol is None
-        loop.run_until_complete(server.listen(8469))
+        event_loop.run_until_complete(server.listen(8469))
         assert isinstance(server.protocol, KademliaProtocol)
         server.stop()
 
-    def test_custom_protocol(self):  # pylint: disable=no-self-use
+    def test_custom_protocol(self, event_loop):  # pylint: disable=no-self-use
         """
         A subclass of Server which overrides the protocol_class attribute will
         have an instance of that class as its protocol after its listen()
@@ -305,15 +306,14 @@ class TestSwappableProtocol:
             protocol_class = CoconutProtocol
 
         # An ordinary server does NOT have a CoconutProtocol as its protocol...
-        loop = asyncio.get_event_loop()
         server = Server()
-        loop.run_until_complete(server.listen(8469))
+        event_loop.run_until_complete(server.listen(8467))
         assert not isinstance(server.protocol, CoconutProtocol)
         server.stop()
 
         # ...but our custom server does.
         husk_server = HuskServer()
-        loop.run_until_complete(husk_server.listen(8469))
+        event_loop.run_until_complete(husk_server.listen(8467))
         assert isinstance(husk_server.protocol, CoconutProtocol)
         husk_server.stop()
 
