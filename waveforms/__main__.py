b715e56b26564a0f959ab2d0951e5e1ee4d4fbec
feihoo87 <xfh1987@gmail.com>
2024-08-13 16:00:41 +0800

    rm codes

--- waveforms/__main__.py
+++ waveforms/__main__.py
@@ -1,8 +1,6 @@
 import click
 
-from .server.__main__ import main as server
 from .sys.net.cli import dht
-from .visualization.__main__ import plot
 
 
 @click.group()
@@ -16,8 +14,6 @@ def hello():
     click.echo('hello, world')
 
 
-main.add_command(plot)
-main.add_command(server)
 main.add_command(dht)
 
 if __name__ == '__main__':
