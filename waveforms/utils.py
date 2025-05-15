3ef04b9b9cc1a15d358af84b865242d25fab104d
feihoo87 <xfh1987@gmail.com>
2025-05-15 11:34:49 +0800

    add freeze

--- waveforms/utils.py
+++ waveforms/utils.py
@@ -1,7 +1,35 @@
 from itertools import repeat
+from types import MappingProxyType
 from typing import Optional, Sequence
 
 import numpy as np
+import scipy.sparse as sp
+
+
+def freeze(x):
+    """Freeze a mutable object.
+    """
+    if isinstance(x, (int, float, complex, str, bytes, type(None))):
+        pass
+    elif isinstance(x, (list, tuple)):
+        return tuple([freeze(y) for y in x])
+    elif isinstance(x, dict):
+        return MappingProxyType({k: freeze(v) for k, v in x.items()})
+    elif isinstance(x, set):
+        return frozenset([freeze(y) for y in x])
+    elif isinstance(x, (np.ndarray, np.matrix)):
+        x.flags.writeable = False
+    elif isinstance(x, sp.spmatrix):
+        x.data.flags.writeable = False
+        if x.format in {'csr', 'csc', 'bsr'}:
+            x.indices.flags.writeable = False
+            x.indptr.flags.writeable = False
+        elif x.format == 'coo':
+            x.row.flags.writeable = False
+            x.col.flags.writeable = False
+    elif isinstance(x, bytearray):
+        x = bytes(x)
+    return x
 
 
 def getFTMatrix(fList: Sequence[float],
